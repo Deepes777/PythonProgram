b01ec12ae574a1694f5f9f3f5e58d7f4bf16d724
Deepes777 <deepesgp@gmail.com>
2022-02-12 15:46:03 +0530

    Code on dynamic typing

---
+++ DynamicTyping.py
@@ -0,0 +1,13 @@
+student = "Bob"
+print(student)
+
+student = [ "Hary", "Ali"]
+print(student)
+
+#Pros of Dynamic Typing
+#1.very easy to work with
+#2.faster development time
+
+#Cons of Dynamic Typing
+#1.may result in unexpected bugs!
+#2.you need to be aware of type()
